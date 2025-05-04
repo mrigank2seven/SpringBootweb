e7ff46a27b67b653b164f5dfc3097d085cea3f66
Mrigank Gupta <mrigank.gupta@progcap.com>
2025-05-04 20:50:33 +0530

    First Commit

---
+++ src/main/java/com/telusko/springbootweb/SpringBootWebApplication.java
@@ -0,0 +1,16 @@
+package com.telusko.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringBootWebApplication.class, args);
+
+        context.getBean(HomeController.class);
+        System.out.println("Spring Boot Application.");
+    }
+}
